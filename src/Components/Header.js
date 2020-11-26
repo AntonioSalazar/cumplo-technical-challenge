c82acfd395669af36833daaccd5272f7815c0b2a
antonioProtopia <antonio@protopia.co>
2020-11-26 01:30:40 -0600

    working on the navbar, able to login and logout now

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../Context/UserProvider'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+  } from 'react-router-dom';
+import logo from '../bank-online.png'
 
 const Header = () => {
-    return (
-        <header>
-            <nav>
 
-            </nav>
+    const { user, loginUser, logoutUser } = useContext(UserContext)
+    return (
+        
+        <header className='header'>
+            <div className="container">
+                <Router>
+                        <div className="header__logo">
+                            <figure>
+                                <img src={logo} alt="bank-cumplo"/>
+                            </figure>
+                        </div>
+
+                        <div className="header__nav">
+                            <Link to='/sucursales'>
+                                Sucursales
+                            </Link>
+                            <Link to='/nosotros'>
+                                Nosotros
+                            </Link>
+
+
+                            {
+                                user.state ? (
+                                    <button
+                                        type='button'
+                                        className='header__logout-btn'
+                                        onClick={logoutUser}
+                                    >Cerrar Sesion</button>
+                                ) : (
+                                    <button
+                                        type='button'
+                                        className='header__login-btn'
+                                        onClick={loginUser}
+                                    >Inicia Sesion</button>
+                                )
+                            }
+
+
+
+                        </div>
+                </Router>
+            </div>
         </header>
     )
 }
--- src/Context/UserProvider.js
+++ src/Context/UserProvider.js
@@ -1,10 +1,59 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
+import { auth, provider } from '../firebase'
 
 export const UserContext = createContext();
 
 const UserProvider = props => {
+
+    const dataUser = {
+        uid: null,
+        email: null,
+        state: null
+    }
+    const [ user, setUser ] = useState(dataUser);
+
+    useEffect(() => {
+        detectUser()
+    }, [])
+
+    const detectUser = () => {
+        auth.onAuthStateChanged(user => {
+            if(user){
+                setUser({
+                    uid: user.uid,
+                    email: user.uid,
+                    state: true
+                })
+            } else {
+                setUser({
+                    uid: null,
+                    email: null,
+                    state: false
+                })
+            }
+        })
+    }
+
+    const loginUser = async() => {
+        try {
+            await auth.signInWithPopup(provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const logoutUser = () => {
+        auth.signOut()
+    }
+
     return (
-        <UserContext.Provider>
+        <UserContext.Provider
+            value={{
+                user,
+                loginUser,
+                logoutUser
+            }}
+        >
             {props.children}
         </UserContext.Provider>
     )
