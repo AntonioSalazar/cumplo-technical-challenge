b18e3753fa3344a3a53706c7860075b39d805fa9
antonioProtopia <antonio@protopia.co>
2020-11-27 00:47:28 -0600

    able to display the banks now, component finished

--- src/Components/Banks.js
+++ src/Components/Banks.js
@@ -9,73 +9,26 @@ import bankImg from '../img/bank.jpg'
 
 const Banks = () => {
 
-    const { banks, error } = useContext(BankContext);
+    const { banks } = useContext(BankContext);
 
     return (
             <section className='banks'>
                 <div className="banks__container">
                     <div className="banks__cards-container">
-                        <div className=" banks__cards-each card">
-                            <img src={bankImg} alt="Bank"/>
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Card title
-                                </h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
-                                <Link to='/'>Ver Sucursales</Link>
-                            </div>
-                        </div>
-                        <div className=" banks__cards-each card">
-                            <img src={bankImg} alt="Bank"/>
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Card title
-                                </h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
-                                <Link to='/'>Ver Sucursales</Link>
-                            </div>
-                        </div>
-                        <div className=" banks__cards-each card">
-                            <img src={bankImg} alt="Bank"/>
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Card title
-                                </h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
-                                <Link to='/'>Ver Sucursales</Link>
-                            </div>
-                        </div>
-                        <div className=" banks__cards-each card">
-                            <img src={bankImg} alt="Bank"/>
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Card title
-                                </h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
-                                <Link to='/'>Ver Sucursales</Link>
-                            </div>
-                        </div>
-                        <div className=" banks__cards-each card">
-                            <img src={bankImg} alt="Bank"/>
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Card title
-                                </h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
-                                <Link to='/'>Ver Sucursales</Link>
-                            </div>
-                        </div>
-                        <div className=" banks__cards-each card">
-                            <img src={bankImg} alt="Bank"/>
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Card title
-                                </h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
-                                <Link to='/'>Ver Sucursales</Link>
-                            </div>
-                        </div>
-
+                        {
+                            banks.map(eachBank => 
+                                 (
+                                    <div className=" banks__cards-each card">
+                                        <img src={bankImg} alt="Bank"/>
+                                        <div className='card-body'>
+                                            <h5 className="card-title">{eachBank.name}</h5>
+                                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis error a similique molestiae quaerat eveniet?</p>
+                                            <Link to='/'>Ver Sucursales</Link>
+                                        </div>
+                                        </div>
+                                )
+                            )
+                        }
                     </div>
                 </div>
             </section>
